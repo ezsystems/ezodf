9c768ffb8e35911fdb2bea71c51313b96bca09e7
Jan Borsodi <jb@ez.no>
2005-01-26 16:11:18 +0000

    - Updated copyrights to include 2005.
    git-svn-id: http://svn.ez.no/svn/extensions/oo/trunk@26 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- modules/oo/ezooconverter.php
+++ modules/oo/ezooconverter.php
@@ -4,7 +4,7 @@
 //
 // Created on: <21-Jan-2005 09:52:07 bf>
 //
-// Copyright (C) 1999-2004 eZ systems as. All rights reserved.
+// Copyright (C) 1999-2005 eZ systems as. All rights reserved.
 //
 // This source file is part of the eZ publish (tm) Open Source Content
 // Management System.
