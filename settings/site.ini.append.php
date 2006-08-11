6f62db6ee858e10fe01d42cbe3d193505d96a0f6
Lukasz Serwatka <ls@ez.no>
2006-08-11 10:52:18 +0000

    - Added AnonymousAccessList[] for oo modules
    git-svn-id: http://svn.ez.no/svn/extensions/oo/trunk@242 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- settings/site.ini.append.php
+++ settings/site.ini.append.php
@@ -1,7 +1,11 @@
 <?php /*
 
-[RoleSettings]
+[SiteAccessSettings]
+AnonymousAccessList[]=oo/upload_import
+AnonymousAccessList[]=oo/authentate
+AnonymousAccessList[]=oo/upload_export
 
+[RoleSettings]
 PolicyOmitList[]=oo/upload_import
 PolicyOmitList[]=oo/authentate
 PolicyOmitList[]=oo/upload_export
