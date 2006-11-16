91f9466fa7b3e961b9762f07f409c89077872211
Valentin Doroschuk <vd@ez.no>
2006-11-16 17:58:23 +0000

    # Added ezinfo.php
    git-svn-id: http://svn.ez.no/svn/extensions/ezodf/stable/2.0@552 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

---
+++ ezinfo.php
@@ -0,0 +1,14 @@
+<?
+class ezdhtmlInfo
+{
+    function info()
+    {
+        return array(
+            'Name' => "eZ OpenOffice.org extension",
+            'Version' => "2.0.0",
+            'Copyright' => "Copyright (C) 1999-2006 eZ systems AS",
+            'License' => "GNU General Public License v2.0",
+     );
+    }
+}
+?>
