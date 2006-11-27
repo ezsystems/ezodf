15eebb442573b29cbffe95178f8607e8cc7c160f
Valentin Doroschuk <vd@ez.no>
2006-11-27 09:05:28 +0000

    - Fixed bug: uses PHP short open tags.
    git-svn-id: http://svn.ez.no/svn/extensions/ezodf/stable/2.0@588 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- ezinfo.php
+++ ezinfo.php
@@ -1,18 +1,17 @@
-<?
+<?php
 class ezodfInfo
 {
     function info()
     {
-        return array(
-            'Name' => "eZ OpenOffice.org extension",
-            'Version' => "2.0.0",
-            'Copyright' => "Copyright (C) 1999-2006 eZ systems AS",
-            'License' => "GNU General Public License v2.0",
-            'Includes the following third-party software' => array( 'Name' => 'PhpConcept Library - Zip Module',
-                                                                    'Version' => '2.4',
-                                                                    'License' => 'GNU/LGPL - Vincent Blavet - November 2004',
-                                                                    'For more information' => '<a href=http://www.phpconcept.net>http://www.phpconcept.net</a>' )
-     );
+        return array( 'Name' => "eZ OpenOffice.org extension",
+                      'Version' => "2.0.0",
+                      'Copyright' => "Copyright (C) 1999-2006 eZ systems AS",
+                      'License' => "GNU General Public License v2.0",
+                      'Includes the following third-party software' => array( 'Name' => 'PhpConcept Library - Zip Module',
+                                                                              'Version' => '2.4',
+                                                                              'License' => 'GNU/LGPL - Vincent Blavet - November 2004',
+                                                                              'For more information' => '<a href=http://www.phpconcept.net>http://www.phpconcept.net</a>' )
+                      );
     }
 }
 ?>
