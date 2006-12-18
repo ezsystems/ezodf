deb037d9d9923eb4751abba4bd3ab2a71ec52526
Valentin Doroschuk <vd@ez.no>
2006-12-18 16:20:00 +0000

    # Fixed version
    git-svn-id: http://svn.ez.no/svn/extensions/ezodf/stable/2.0@718 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- ezinfo.php
+++ ezinfo.php
@@ -29,7 +29,7 @@ class ezodfInfo
     function info()
     {
         return array( 'Name' => "eZ OpenOffice.org extension",
-                      'Version' => "2.0.0",
+                      'Version' => "2.0.1",
                       'Copyright' => "Copyright (C) 1999-2006 eZ systems AS",
                       'License' => "GNU General Public License v2.0",
                       'Includes the following third-party software' => array( 'Name' => 'PhpConcept Library - Zip Module',
