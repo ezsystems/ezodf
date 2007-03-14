dccf77408ebcd4eadae4122caafc10474d6fd259
Lukasz Serwatka <ls@ez.no>
2007-03-14 13:59:13 +0000

    - Updated German translation
    git-svn-id: http://svn.ez.no/svn/extensions/ezodf/trunk@1028 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- translations/ger-DE/translation.ts
+++ translations/ger-DE/translation.ts
@@ -5,77 +5,77 @@
     <message>
         <location filename="" line="0"/>
         <source>OpenOffice.org export</source>
-        <translation type="unfinished">OpenOffice.org Export</translation>
+        <translation>OpenOffice.org Export</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Export eZ publish content to OpenOffice.org</source>
-        <translation type="unfinished">Inhalte aus eZ publish nach OpenOffice.org exportieren</translation>
+        <translation>Inhalte aus eZ publish nach OpenOffice.org exportieren</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Error</source>
-        <translation type="unfinished">Fehler</translation>
+        <translation>Fehler</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Here you can export any eZ publish content object to an OpenOffice.org Writer document format.</source>
-        <translation type="unfinished">Hier k&amp;ouml;nnen Sie beliebigen Inhalt aus eZ publish in das OpenOffice.org Writer Format exportieren</translation>
+        <translation>Hier können Sie beliebigen Inhalt aus eZ publish in das OpenOffice.org Writer Format exportieren.</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Export Object</source>
-        <translation type="unfinished">Objekt exportieren</translation>
+        <translation>Objekt exportieren</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Document is now imported</source>
-        <translation type="unfinished">Dokument wurde importiert</translation>
+        <translation>Dokument wurde importiert</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>OpenOffice.org import</source>
-        <translation type="unfinished">OpenOffice.org Import</translation>
+        <translation>OpenOffice.org Import</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>The object was imported as: %class_name</source>
-        <translation type="unfinished">Das Objekt wurde importiert als: %class_name</translation>
+        <translation>Das Objekt wurde importiert als: %class_name</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Document imported as</source>
-        <translation type="unfinished">Das Objekt wurde importiert als</translation>
+        <translation>Das Objekt wurde importiert als</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>The images are placed in the media and can be re-used.</source>
-        <translation type="unfinished">Die Bilder wurden im Media Bereich gespeichert und k&amp;ouml;nnen wiederverwendet werden.</translation>
+        <translation>Die Bilder wurden im Media Bereich gespeichert und können wiederverwendet werden.</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Import another document</source>
-        <translation type="unfinished">Weiteres Dokument importieren</translation>
+        <translation>Weiteres Dokument importieren</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Upload file</source>
-        <translation type="unfinished">Datei hochladen</translation>
+        <translation>Datei hochladen</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Import OpenOffice.org document</source>
-        <translation type="unfinished">OpenOffice.org Dokument importieren</translation>
+        <translation>OpenOffice.org Dokument importieren</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Replace document</source>
-        <translation type="unfinished">Dokument ersetzen</translation>
+        <translation>Dokument ersetzen</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Import to</source>
-        <translation type="unfinished">Importieren nach</translation>
+        <translation>Importieren nach</translation>
     </message>
     <message>
         <location filename="" line="0"/>
@@ -83,9 +83,9 @@
 asked where to place the document and eZ publish does the rest. The document is converted into
 the appropriate class during the import, you get a notice about this after the import is done.
 Images are placed in the media library so you can re-use them in other articles.</source>
-        <translation type="unfinished">Sie k&amp;ouml;nnen OpenOffice.org Writer Dokumente direkt in eZ publish importieren. 
-Sie werden gefragt wo das Dokument gespeichert werden soll und eZ publish startet die verarbeitung. Das Dokument wird in eine passende Klasse konvertiert, 
-Sie erhalten eine Benachrichtigung dar&amp;uuml;ber, wenn der Import abgeschlossen ist. Bilder werden im Media Bereich gespeichert und k&amp;ouml;nnen somit weiter verwendet werden.</translation>
+        <translation>Sie können OpenOffice.org Writer Dokumente direkt in eZ publish importieren. 
+Sie werden gefragt wo das Dokument gespeichert werden soll und eZ publish startet die Verarbeitung. Das Dokument wird in eine passende Klasse konvertiert, 
+Sie erhalten eine Benachrichtigung darüber, wenn der Import abgeschlossen ist. Bilder werden im Media Bereich gespeichert und können somit weiter verwendet werden.</translation>
     </message>
 </context>
 <context>
@@ -93,7 +93,7 @@ Sie erhalten eine Benachrichtigung dar&amp;uuml;ber, wenn der Import abgeschloss
     <message>
         <location filename="" line="0"/>
         <source>Choose document placement</source>
-        <translation type="unfinished">Bitte einen Ort f&amp;uuml;r das Dokument w&amp;auml;hlen</translation>
+        <translation>Bitte einen Ort für das Dokument wählen</translation>
     </message>
     <message>
         <location filename="" line="0"/>
@@ -102,16 +102,16 @@ Sie erhalten eine Benachrichtigung dar&amp;uuml;ber, wenn der Import abgeschloss
     Select the placements and click the %buttonname button.
     Using the recent and bookmark items for quick placement is also possible.
     Click on placement names to change the browse listing.</source>
-        <translation type="unfinished">Bitte w&amp;auml;hlen Sie den Ort f&amp;uuml;r das OpenOffice.org Objekt.
+        <translation>Bitte wählen Sie den Ort für das OpenOffice.org Objekt.
 
-Ort ausw&amp;auml;hlen und %buttonname dr&amp;uuml;cken.
-Die Nutzung von aktuellen Elementen und Lesezeichen f&amp;uuml; die schnelle Plazierung m&amp;ouml;glich
-Klicken Sie auf den Namen der Plazierung um die Auflistung des Browsers zu &amp;auml;ndern.</translation>
+Ort auswählen und %buttonname drücken.
+Die Nutzung von aktuellen Elementen und Lesezeichen für die schnelle Plazierung möglich
+Klicken Sie auf den Namen der Plazierung um die Auflistung des Browsers zu ändern.</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Select</source>
-        <translation type="unfinished">Ausw&amp;auml;hlen</translation>
+        <translation>Auswählen</translation>
     </message>
 </context>
 <context>
@@ -119,27 +119,27 @@ Klicken Sie auf den Namen der Plazierung um die Auflistung des Browsers zu &amp;
     <message>
         <location filename="" line="0"/>
         <source>Destination file format not supported</source>
-        <translation type="unfinished">Zielformat wird nicht unterst&amp;uuml;tzt</translation>
+        <translation>Zielformat wird nicht unterstützt</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>PDF conversion failed</source>
-        <translation type="unfinished">PDF Konvertierung fehlgeschlagen</translation>
+        <translation>PDF Konvertierung fehlgeschlagen</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Word conversion failed</source>
-        <translation type="unfinished">Word Konvertierung fehlgeschlagen</translation>
+        <translation>Word Konvertierung fehlgeschlagen</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Unable to fetch node, or no read access</source>
-        <translation type="unfinished">Kann Knoten nicht gelesen werden, oder kein Zugriff</translation>
+        <translation>Kann Knoten nicht gelesen werden, oder kein Zugriff</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Unable to open file %1 on server side</source>
-        <translation type="unfinished">Serverseitiges &amp;Ouml;ffnen der Datei %1 nicht m&amp;ouml;glich</translation>
+        <translation>Serverseitiges Öffnen der Datei %1 nicht möglich</translation>
     </message>
 </context>
 <context>
@@ -147,92 +147,92 @@ Klicken Sie auf den Namen der Plazierung um die Auflistung des Browsers zu &amp;
     <message>
         <location filename="" line="0"/>
         <source>File extension or type is not allowed.</source>
-        <translation type="unfinished">Die Dateierweiterung oder der Dateityp ist nicht zugelassen </translation>
+        <translation>Die Dateierweiterung oder der Dateityp ist nicht zugelassen.</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Could not parse XML.</source>
-        <translation type="unfinished">Konnte XML nicht verarbeiten</translation>
+        <translation>Konnte XML nicht verarbeiten.</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Can not open socket. Please check if extension/ezodf/deamon.php is running.</source>
-        <translation type="unfinished">Konnte Socket nicht &amp;ouml;ffnen. Bitte pr&amp;uuml;fen Sie ob der Dienst extension/ezodf/deamon.php aktiv ist.</translation>
+        <translation>Konnte Socket nicht öffnen. Bitte prüfen Sie ob der Dienst extension/ezodf/deamon.php aktiv ist.</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Can not convert the given document.</source>
-        <translation type="unfinished">Kann nicht in das gew&amp;auml;hlte Format konvertieren.</translation>
+        <translation>Kann nicht in das gewählte Format konvertieren.</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Unable to call deamon. Fork can not create child process.</source>
-        <translation type="unfinished">Kann den Dienst nicht aufrufen. Die Verzweigung kann den Prozess nicht erstellen.</translation>
+        <translation>Kann den Dienst nicht aufrufen. Die Verzweigung kann den Prozess nicht erstellen.</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Deamon reported error.</source>
-        <translation type="unfinished">Der Dienst hat einen Fehler gemeldet.</translation>
+        <translation>Der Dienst hat einen Fehler gemeldet.</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Unknown node.</source>
-        <translation type="unfinished">Unbekannter Knoten.</translation>
+        <translation>Unbekannter Knoten.</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Access denied.</source>
-        <translation type="unfinished">Zugriff verweigert.</translation>
+        <translation>Zugriff verweigert.</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Error during import.</source>
-        <translation type="unfinished">Ein Fehler ist w&amp;auml;hrend des Imports aufgetreten.</translation>
+        <translation>Ein Fehler ist während des Imports aufgetreten.</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Unknown content class specified in odf.ini:</source>
-        <translation type="unfinished">Eine Unbekannte Inhaltsklasse wurde in der odf.ini angegeben.</translation>
+        <translation>Eine Unbekannte Inhaltsklasse wurde in der odf.ini angegeben:</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Unknown error.</source>
-        <translation type="unfinished">Unbekannter Fehler.</translation>
+        <translation>Unbekannter Fehler.</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Filetype: </source>
-        <translation type="unfinished">Dateityp: </translation>
+        <translation>Dateityp: </translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Unable to fetch node with id  </source>
-        <translation type="unfinished">  Kann Knoten nicht aufrufen, Knoten id</translation>
+        <translation>  Kann Knoten nicht aufrufen, Knoten id</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Folder for images could not be created, access denied.</source>
-        <translation type="unfinished">Der Ordner f&amp;uuml;r die Bilder konnte nicht erstellt werden. Zugriff verweigert.</translation>
+        <translation>Der Ordner für die Bilder konnte nicht erstellt werden. Zugriff verweigert.</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Document is not suported.</source>
-        <translation type="unfinished">Dokument wird nicht unterst&amp;uuml;tzt</translation>
+        <translation>Dokument wird nicht unterstützt.</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Cannot import. File not found. Already imported?</source>
-        <translation type="unfinished">Konnte nicht importieren. Datei nicht gefunden. Bereits importiert?</translation>
+        <translation>Konnte nicht importieren. Datei nicht gefunden. Bereits importiert?</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Cannot import document, supplied placement nodeID is not valid.</source>
-        <translation type="unfinished">Kann Dokument nicht importieren, angegebene KnotenID f&amp;uuml;r die Plazierung ist nicht g&amp;uuml;ltig.</translation>
+        <translation>Kann Dokument nicht importieren, angegebene KnotenID für die Plazierung ist nicht gültig.</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Cannot store uploaded file, cannot import.</source>
-        <translation type="unfinished">Kann hochgeladene Datei nicht speichern. Import nicht m&amp;ouml;glich.</translation>
+        <translation>Kann hochgeladene Datei nicht speichern. Import nicht möglich.</translation>
     </message>
 </context>
 <context>
@@ -240,27 +240,27 @@ Klicken Sie auf den Namen der Plazierung um die Auflistung des Browsers zu &amp;
     <message>
         <location filename="" line="0"/>
         <source>Export OpenOffice.org</source>
-        <translation type="unfinished">OpenOffice.org exportieren</translation>
+        <translation>OpenOffice.org exportieren</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Export PDF</source>
-        <translation type="unfinished">PDF exportieren</translation>
+        <translation>PDF exportieren</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Export Word</source>
-        <translation type="unfinished">Word exportieren</translation>
+        <translation>Word exportieren</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Import OpenOffice.org</source>
-        <translation type="unfinished">OpenOffice.org importieren</translation>
+        <translation>OpenOffice.org importieren</translation>
     </message>
     <message>
         <location filename="" line="0"/>
         <source>Replace OpenOffice.org</source>
-        <translation type="unfinished">OpenOffice.org ersetzen</translation>
+        <translation>OpenOffice.org ersetzen</translation>
     </message>
 </context>
 </TS>
