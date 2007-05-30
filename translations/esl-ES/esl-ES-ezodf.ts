f1e85194c4abb043fdc707c49bf174a9de102cb6
Ole Marius Smestad <oms@ez.no>
2007-05-30 09:08:58 +0000

    - Adding esl-ES translation.
    git-svn-id: http://svn.ez.no/svn/extensions/ezodf/stable/2.0@1457 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

---
+++ translations/esl-ES/esl-ES-ezodf.ts
@@ -0,0 +1,229 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="es_ES">
+<context>
+    <name>extension/ezodf</name>
+    <message>
+        <source>Import</source>
+        <translation>Importar</translation>
+    </message>
+    <message>
+        <source>Export</source>
+        <translation>Exportar</translation>
+    </message>
+    <message>
+        <source>Replace</source>
+        <translation>Reemplazar</translation>
+    </message>
+    <message>
+        <source>Choose document placement</source>
+        <translation>Elegir emplazamiento al documento</translation>
+    </message>
+    <message>
+        <source>Upload file</source>
+        <translation>Cargar fichero</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Error</translation>
+    </message>
+    <message>
+        <source>Export Object</source>
+        <translation>Exportar Objeto</translation>
+    </message>
+    <message>
+        <source>OpenOffice.org import</source>
+        <translation>Importar OpenOffice.org</translation>
+    </message>
+    <message>
+        <source>OpenOffice.org export</source>
+        <translation>Exportar OpenOffice.org</translation>
+    </message>
+    <message>
+        <source>Export eZ publish content to OpenOffice.org</source>
+        <translation>Exportar contenido del eZ publish a OpenOffice.org</translation>
+    </message>
+    <message>
+        <source>Here you can export any eZ publish content object to an OpenOffice.org Writer document format.</source>
+        <translation>Aquí puedes exportar cualquier objeto de contenido del eZ publish a un documento en formato OpenOffice.org Writer.</translation>
+    </message>
+    <message>
+        <source>Document is now imported</source>
+        <translation>El documento se imprta ahora</translation>
+    </message>
+    <message>
+        <source>The object was imported as: %class_name</source>
+        <translation>El objeto fue importado como: %class_name</translation>
+    </message>
+    <message>
+        <source>Document imported as</source>
+        <translation>Documento importado como</translation>
+    </message>
+    <message>
+        <source>The images are placed in the media and can be re-used.</source>
+        <translation>Las imágenes se ubican en biblioteca multimedia y pueden ser reutilizadas. </translation>
+    </message>
+    <message>
+        <source>Import another document</source>
+        <translation>Importar otro documento</translation>
+    </message>
+    <message>
+        <source>Import OpenOffice.org document</source>
+        <translation>Importar documento OpenOffice.org</translation>
+    </message>
+    <message>
+        <source>Replace document</source>
+        <translation>Reemplazar documento</translation>
+    </message>
+    <message>
+        <source>Import to</source>
+        <translation>Importar a</translation>
+    </message>
+    <message>
+        <source>You can import OpenOffice.org Writer documents directly into eZ publish from this page. You are
+asked where to place the document and eZ publish does the rest. The document is converted into
+the appropriate class during the import, you get a notice about this after the import is done.
+Images are placed in the media library so you can re-use them in other articles.</source>
+        <translation type="unfinished">Puedes importar los documentos OpenOffice.org Writer directamente al eZ publish desde esta página. El sistema te preguntará donde quieres ubicar el documento y el eZ publish hará el resto. El documento es convertido a la clase apropiada durante la importación, y se mostrará un aviso después de la importación. Las imágenes se ubican en la biblioteca multimedia y poder reutilizarlas en otros artículos. </translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezodf/browse</name>
+    <message>
+        <source>Choose document placement</source>
+        <translation>Elegir emplazamiento al documento</translation>
+    </message>
+    <message>
+        <source>Please choose the placement for the OpenOffice.org object.
+
+    Select the placements and click the %buttonname button.
+    Using the recent and bookmark items for quick placement is also possible.
+    Click on placement names to change the browse listing.</source>
+        <translation>Elige la ubicación para el objeto OpenOffice.org.    Seleccionar ubicación y pulsar %buttonname button.    Usar los artículos recientes y bookmark para la ubicación rápida es también posible.    Pulsar en el nombre de la ubicación para cambiar el listado de navegación.</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation>Seleccionar</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezodf/export/error</name>
+    <message>
+        <source>Destination file format not supported</source>
+        <translation>Formato del archivo de destino no soportado</translation>
+    </message>
+    <message>
+        <source>PDF conversion failed</source>
+        <translation>Conversión a PDF fallida</translation>
+    </message>
+    <message>
+        <source>Word conversion failed</source>
+        <translation>Conversión a Word fallida</translation>
+    </message>
+    <message>
+        <source>Unable to fetch node, or no read access</source>
+        <translation>INcapaz de traer el nodo, o no dispones de acceso de lectura</translation>
+    </message>
+    <message>
+        <source>Unable to open file %1 on server side</source>
+        <translation>Incapaz de abrir el archivo %1 en el lado del servidor </translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezodf/import/error</name>
+    <message>
+        <source>File extension or type is not allowed.</source>
+        <translation>La extensión edl fichero o el tipo no es soportado.</translation>
+    </message>
+    <message>
+        <source>Could not parse XML.</source>
+        <translation>No puede alanizar el XML.</translation>
+    </message>
+    <message>
+        <source>Can not open socket. Please check if extension/ezodf/deamon.php is running.</source>
+        <translation>No puede abrirse e socket. Compruebe si extension/ezodf/deamon.php está funcionando.</translation>
+    </message>
+    <message>
+        <source>Can not convert the given document.</source>
+        <translation>No puede convertirse el documento. </translation>
+    </message>
+    <message>
+        <source>Unable to call deamon. Fork can not create child process.</source>
+        <translation>Incapaz de llamar el deamon. La bifurcación no puede crear un proceso hijo. </translation>
+    </message>
+    <message>
+        <source>Deamon reported error.</source>
+        <translation>Error divulgado Deamon. </translation>
+    </message>
+    <message>
+        <source>Unknown node.</source>
+        <translation>Nodo desconocido</translation>
+    </message>
+    <message>
+        <source>Access denied.</source>
+        <translation>Acceso denegado</translation>
+    </message>
+    <message>
+        <source>Error during import.</source>
+        <translation>Error durante la importación</translation>
+    </message>
+    <message>
+        <source>Unknown content class specified in odf.ini:</source>
+        <translation>Clase de contenido desconocida especificada en odf.ini: </translation>
+    </message>
+    <message>
+        <source>Unknown error.</source>
+        <translation>Error desconocido.</translation>
+    </message>
+    <message>
+        <source>Filetype: </source>
+        <translation>Tipo de Fichero: </translation>
+    </message>
+    <message>
+        <source>Unable to fetch node with id  </source>
+        <translation>Incapez de traer el nodo con ID  </translation>
+    </message>
+    <message>
+        <source>Folder for images could not be created, access denied.</source>
+        <translation>La carpeta para las imágenes no se puede crear, tiene acceso negado. </translation>
+    </message>
+    <message>
+        <source>Document is not suported.</source>
+        <translation>El documento no es soportado.</translation>
+    </message>
+    <message>
+        <source>Cannot import. File not found. Already imported?</source>
+        <translation>No puede importar. Archivo no encontrado. ¿Importado ya? </translation>
+    </message>
+    <message>
+        <source>Cannot import document, supplied placement nodeID is not valid.</source>
+        <translation>No puede importar el documento, el nodeID proveído de ubicación es inválido. </translation>
+    </message>
+    <message>
+        <source>Cannot store uploaded file, cannot import.</source>
+        <translation>No puede almacenar el archivo cargado, no se puede importar. </translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezodf/popupmenu</name>
+    <message>
+        <source>Export OpenOffice.org</source>
+        <translation>Exportar OpenOffice.org</translation>
+    </message>
+    <message>
+        <source>Export PDF</source>
+        <translation>Exportar a PDF</translation>
+    </message>
+    <message>
+        <source>Export Word</source>
+        <translation>Exportar a Word</translation>
+    </message>
+    <message>
+        <source>Import OpenOffice.org</source>
+        <translation>Importar de Office.org</translation>
+    </message>
+    <message>
+        <source>Replace OpenOffice.org</source>
+        <translation>Reemplazar OpenOffice.org</translation>
+    </message>
+</context>
+</TS>
