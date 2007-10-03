a48d5754820377a55b038a31052c1cc73daa2b68
Dmytro Lakhtyuk <dl@ez.no>
2007-10-03 20:41:35 +0000

    - Added missing file.
    git-svn-id: http://svn.ez.no/svn/extensions/ezodf/unstable/ezodf_php5@1895 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

---
+++ dist.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+EXTENSION_NAME="eZ OpenOffice.org extension"
+EXTENSION_IDENTIFIER="ezodf"
+EXTENSION_SUMMARY="This extension enables import and export of OpenOffice.org Writer documents within eZ publish"
+EXTENSION_LICENSE="eZ Proprietary License"
+EXTENSION_VERSION="2.0.1"
+EXTENSION_PUBLISH_VERSION="3.9 and higher"
+EXTENSION_ARCHIVE_NAME="ezodf"
+EXTENSION_PHP_VERSION="4.4"
+
+EXTENSION_FILTER_FILES="README"
