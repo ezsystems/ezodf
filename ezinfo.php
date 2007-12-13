623e7c1118e25ec50ee19c87ae2df38371cce4c1
Lukasz Serwatka <ls@ez.no>
2007-12-13 07:51:30 +0000

    - Updated version.
    git-svn-id: http://svn.ez.no/svn/extensions/ezodf/stable/2.1@2413 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- ezinfo.php
+++ ezinfo.php
@@ -29,7 +29,7 @@ class ezodfInfo
     static function info()
     {
         return array( 'Name' => "eZ OpenOffice.org extension",
-                      'Version' => "2.0.1",
+                      'Version' => "2.1.0",
                       'Copyright' => "Copyright (C) 1999-2006 eZ systems AS",
                       'License' => "GNU General Public License v2.0",
                       'Includes the following third-party software' => array( 'Name' => 'PhpConcept Library - Zip Module',
