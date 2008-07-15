b28405be5037a927e63411f0dc9ca7eacfc67f95
Lukasz Serwatka <ls@ez.no>
2008-07-15 09:35:06 +0000

    - Added nor-NO translation
    git-svn-id: http://svn.ez.no/svn/extensions/ezodf/trunk@3113 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

---
+++ translations/nor-NO/translation.ts
@@ -0,0 +1,223 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>extension/ezodf</name>
+    <message>
+        <source>OpenOffice.org export</source>
+        <translation>OpenOffice.org eksport</translation>
+    </message>
+    <message>
+        <source>Export eZ Publish content to OpenOffice.org</source>
+        <translation>Eksporter innhold fra eZ Publish til OpenOffice.org</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Feil</translation>
+    </message>
+    <message>
+        <source>Here you can export any eZ Publish content object to an OpenOffice.org Writer document format.</source>
+        <translation>Her kan du eksportere fra innholdsobjekter i eZ Publish til OpenOffice.org Writer sitt dokumentformat.</translation>
+    </message>
+    <message>
+        <source>Export Object</source>
+        <translation>Eksporter objekt</translation>
+    </message>
+    <message>
+        <source>Document is now imported</source>
+        <translation>Doumentet er importert</translation>
+    </message>
+    <message>
+        <source>OpenOffice.org import</source>
+        <translation>OpenOffice.org import</translation>
+    </message>
+    <message>
+        <source>The object was imported as: %class_name</source>
+        <translation>Objektet er importert som: %class_name</translation>
+    </message>
+    <message>
+        <source>Document imported as</source>
+        <translation>Dokument importert som</translation>
+    </message>
+    <message>
+        <source>The images are placed in the media library and can be re-used.</source>
+        <translation>Bildene er plassert i mediebiblioteket og kan gjenbrukes andre steder.</translation>
+    </message>
+    <message>
+        <source>Import another document</source>
+        <translation>Importer et nytt dokument</translation>
+    </message>
+    <message>
+        <source>Upload file</source>
+        <translation>Last opp fil</translation>
+    </message>
+    <message>
+        <source>Import OpenOffice.org document</source>
+        <translation>Importer OpenOffice.org-dokument</translation>
+    </message>
+    <message>
+        <source>Replace document</source>
+        <translation>Erstatt dokument</translation>
+    </message>
+    <message>
+        <source>Import to</source>
+        <translation>Importer til</translation>
+    </message>
+    <message>
+        <source>You can import OpenOffice.org Writer documents directly into eZ Publish from this page. You are
+asked where to place the document and eZ Publish does the rest. The document is converted into
+the appropriate class during the import, you get a notice about this after the import is done.
+Images are placed in the media library so you can re-use them in other articles.</source>
+        <translation>Fra denne siden kan du importere dokumenter fra OpenOffice.org Writer til eZ Publish. Du vil bli
+spurt om hvor du vil plassere dokumentet, og eZ Publish gjør resten. Dokumentet blir konvertert til
+riktig klasse, og du får en melding når importen er ferdig. Bilder blir plassert i mediebiblioteket, slik
+at du kan gjenbruke dem i andre artikler.</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezodf/browse</name>
+    <message>
+        <source>Choose document placement</source>
+        <translation>Velg dokumentplassering</translation>
+    </message>
+    <message>
+        <source>Please choose the placement for the OpenOffice.org object.
+
+    Select the placements and click the %buttonname button.
+    Using the recent and bookmark items for quick placement is also possible.
+    Click on placement names to change the browse listing.</source>
+        <translation>Velg plassering for OpenOffice.org-objektet.
+
+Velg plasseringen og klikk på %buttonname-knappen.
+Bruk nylig valgte og bokmerker for raskere valg.
+Klikk navnene til objektene for å gå igjennom treet.</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation>Velg</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezodf/export/error</name>
+    <message>
+        <source>Destination file format not supported</source>
+        <translation>Filformatet du vil konvertere til er ikke støttet</translation>
+    </message>
+    <message>
+        <source>PDF conversion failed</source>
+        <translation>En feil har oppstått under konvertering til PDF</translation>
+    </message>
+    <message>
+        <source>Word conversion failed</source>
+        <translation>En feil har oppstått under konvertering til Word</translation>
+    </message>
+    <message>
+        <source>Unable to fetch node, or no read access</source>
+        <translation>Kunne ikke hente node, eller mangler lesetilgang</translation>
+    </message>
+    <message>
+        <source>Unable to open file %1 on server side</source>
+        <translation>Kunne ikke åpne filen %1 på serveren</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezodf/import/error</name>
+    <message>
+        <source>File extension or type is not allowed.</source>
+        <translation>Filendelsen eller -typen er ikke tillatt.</translation>
+    </message>
+    <message>
+        <source>Could not parse XML.</source>
+        <translation>Kunne ikke lese XML.</translation>
+    </message>
+    <message>
+        <source>Can not open socket. Please check if extension/ezodf/daemon.php is running.</source>
+        <translation>Kunne ikke åpne socket. Vennligst kontroller om extension/ezodf/daemon.php kjører.</translation>
+    </message>
+    <message>
+        <source>Can not convert the given document.</source>
+        <translation>Kan ikke konvertere dokumentet.</translation>
+    </message>
+    <message>
+        <source>Unable to call daemon. Fork can not create child process.</source>
+        <translation>Kunne ikke starte daemon. Fork kan ikke opprette subprosess.</translation>
+    </message>
+    <message>
+        <source>Daemon reported error.</source>
+        <translation>Daemon rapperterer feil.</translation>
+    </message>
+    <message>
+        <source>Unknown node.</source>
+        <translation>Ukjent node.</translation>
+    </message>
+    <message>
+        <source>Access denied.</source>
+        <translation>Mangler tilgang.</translation>
+    </message>
+    <message>
+        <source>Error during import.</source>
+        <translation>Feil under import.</translation>
+    </message>
+    <message>
+        <source>Unknown content class specified in odf.ini:</source>
+        <translation>Ukjent innholdsklasse spesifisert i odf.ini:</translation>
+    </message>
+    <message>
+        <source>Unknown error.</source>
+        <translation>Ukjent feil.</translation>
+    </message>
+    <message>
+        <source>Filetype: </source>
+        <translation>Filtype: </translation>
+    </message>
+    <message>
+        <source>Unable to fetch node with id  </source>
+        <translation>Kunne ikke hente node med id  </translation>
+    </message>
+    <message>
+        <source>Folder for images could not be created, access denied.</source>
+        <translation>Mappe for bilder kunne ikke opprettes, tilgang nektet.</translation>
+    </message>
+    <message>
+        <source>Document is not supported.</source>
+        <translation>Dokumentet er ikke støttet.</translation>
+    </message>
+    <message>
+        <source>Cannot import. File not found. Already imported?</source>
+        <translation>Kan ikke importere. Filen ble ikke funnet. Er den allerede importert?</translation>
+    </message>
+    <message>
+        <source>Cannot import document, supplied placement nodeID is not valid.</source>
+        <translation>Kan ikke importere dokument, den oppgitte node-IDen er ikke gyldig.</translation>
+    </message>
+    <message>
+        <source>Cannot store uploaded file, cannot import.</source>
+        <translation>Kan ikke lagre opplastet fil, kan ikke importere.</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezodf/popupmenu</name>
+    <message>
+        <source>OpenOffice.org</source>
+        <translation>OpenOffice.org</translation>
+    </message>
+    <message>
+        <source>Export OpenOffice</source>
+        <translation>Eksporter OpenOffice</translation>
+    </message>
+    <message>
+        <source>Export PDF</source>
+        <translation>Eksporter PDF</translation>
+    </message>
+    <message>
+        <source>Export Word</source>
+        <translation>Eksporter Word</translation>
+    </message>
+    <message>
+        <source>Import OpenOffice</source>
+        <translation>Importer OpenOffice</translation>
+    </message>
+    <message>
+        <source>Replace OpenOffice</source>
+        <translation>Erstatt OpenOffice</translation>
+    </message>
+</context>
+</TS>
