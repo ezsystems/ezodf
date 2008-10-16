a716c736fdf413119c38c67be88aac2bb164ca45
Lukasz Serwatka <ls@ez.no>
2008-10-16 08:25:06 +0000

    - Added cat-ES translation
    git-svn-id: http://svn.ez.no/svn/extensions/ezodf/trunk@3256 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

---
+++ translations/cat-ES/translation.ts
@@ -0,0 +1,225 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>extension/ezodf</name>
+    <message>
+        <source>OpenOffice.org export</source>
+        <translation>OpenOffice.org Export</translation>
+    </message>
+    <message>
+        <source>Export eZ Publish content to OpenOffice.org</source>
+        <translation>Exporta eZ Publish content a OpenOffice.org</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Error</translation>
+    </message>
+    <message>
+        <source>Here you can export any eZ Publish content object to an OpenOffice.org Writer document format.</source>
+        <translation>Aquí pots exportar qualsevol objecte eZ Publish content a format document OpenOffice.org Writer. </translation>
+    </message>
+    <message>
+        <source>Export Object</source>
+        <translation>Exporta l&apos;objecte</translation>
+    </message>
+    <message>
+        <source>Document is now imported</source>
+        <translation>S&apos;ha importat el document</translation>
+    </message>
+    <message>
+        <source>OpenOffice.org import</source>
+        <translation>OpenOffice.org import</translation>
+    </message>
+    <message>
+        <source>The object was imported as: %class_name</source>
+        <translation>S&apos;ha importat l&apos;objecte com a: %class_name</translation>
+    </message>
+    <message>
+        <source>Document imported as</source>
+        <translation>S&apos;ha importat el document com a</translation>
+    </message>
+    <message>
+        <source>The images are placed in the media library and can be re-used.</source>
+        <translation>Les imatges són a la mediateca i poden ser reutilitzades.</translation>
+    </message>
+    <message>
+        <source>Import another document</source>
+        <translation>Importa un altre document</translation>
+    </message>
+    <message>
+        <source>Upload file</source>
+        <translation>Carrega el document</translation>
+    </message>
+    <message>
+        <source>Import OpenOffice.org document</source>
+        <translation>Importa document OpenOffice.org</translation>
+    </message>
+    <message>
+        <source>Replace document</source>
+        <translation>Substitueix el docuemnt</translation>
+    </message>
+    <message>
+        <source>Import to</source>
+        <translation>Importa a</translation>
+    </message>
+    <message>
+        <source>You can import OpenOffice.org Writer documents directly into eZ Publish from this page. You are
+asked where to place the document and eZ Publish does the rest. The document is converted into
+the appropriate class during the import, you get a notice about this after the import is done.
+Images are placed in the media library so you can re-use them in other articles.</source>
+        <translation>Pots importar documents OpenOffice.org Writer directament a eZ Publish des d&apos;aquesta pàgina. Has de
+definir on vols ubicar el document i l&apos;eZ Publish fa la resta. El document es converteix al 
+format adecuat durant el procés d&apos;importació. Se te n&apos;informarà un cop el document hagi estat convertit.
+Les imatges es coloquen a la mediateca per tal que puguin ser reutilitzades en altre articles.</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezodf/browse</name>
+    <message>
+        <source>Choose document placement</source>
+        <translation>Selecciona la ubicació del document</translation>
+    </message>
+    <message>
+        <source>Please choose the placement for the OpenOffice.org object.
+
+    Select the placements and click the %buttonname button.
+    Using the recent and bookmark items for quick placement is also possible.
+    Click on placement names to change the browse listing.</source>
+        <translation>Selecciona una ubicació per a l&apos;objecte OpenOffice.org. 
+
+Selecciona les ubicacions i prem aquest %buttonname botó.
+També pots utilitzar ítems i marcadors recents per a una ubicació ràpida.
+Clica sobre el nom de les ubicacions per a canviar la llista de navegació.
+
+</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation>Selecciona</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezodf/export/error</name>
+    <message>
+        <source>Destination file format not supported</source>
+        <translation>Format de destinació de document no suportat</translation>
+    </message>
+    <message>
+        <source>PDF conversion failed</source>
+        <translation>La conversió a PDF no ha funcionat</translation>
+    </message>
+    <message>
+        <source>Word conversion failed</source>
+        <translation>La conversió a Word no ha funcionat</translation>
+    </message>
+    <message>
+        <source>Unable to fetch node, or no read access</source>
+        <translation>No es pot recuperar el nòdul o no es pot llegir</translation>
+    </message>
+    <message>
+        <source>Unable to open file %1 on server side</source>
+        <translation>No es pot obrir el document %1 amb el server side</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezodf/import/error</name>
+    <message>
+        <source>File extension or type is not allowed.</source>
+        <translation>Extensió o tipus de document no permès.</translation>
+    </message>
+    <message>
+        <source>Could not parse XML.</source>
+        <translation>No s&apos;ha pogut analitzar l&apos;XML.</translation>
+    </message>
+    <message>
+        <source>Can not open socket. Please check if extension/ezodf/daemon.php is running.</source>
+        <translation>No es pot obrir la placa base. Comprova si l&apos;extensió /ezodf/daemon.php està operativa.</translation>
+    </message>
+    <message>
+        <source>Can not convert the given document.</source>
+        <translation>Aquest document no es pot convertir.</translation>
+    </message>
+    <message>
+        <source>Unable to call daemon. Fork can not create child process.</source>
+        <translation>No es troba el dimoni. La bifurcació no pot crear el procés fill.</translation>
+    </message>
+    <message>
+        <source>Daemon reported error.</source>
+        <translation>El dimoni dóna error.</translation>
+    </message>
+    <message>
+        <source>Unknown node.</source>
+        <translation>Nòdul desconegut.</translation>
+    </message>
+    <message>
+        <source>Access denied.</source>
+        <translation>Accés denegat.</translation>
+    </message>
+    <message>
+        <source>Error during import.</source>
+        <translation>Error durant la importació.</translation>
+    </message>
+    <message>
+        <source>Unknown content class specified in odf.ini:</source>
+        <translation>La classe de contingut especificada a odf.ini és desconeguda:</translation>
+    </message>
+    <message>
+        <source>Unknown error.</source>
+        <translation>Error desconegut.</translation>
+    </message>
+    <message>
+        <source>Filetype: </source>
+        <translation>Tipus de document:</translation>
+    </message>
+    <message>
+        <source>Unable to fetch node with id  </source>
+        <translation>No es pot recuperar el nòdul amb id</translation>
+    </message>
+    <message>
+        <source>Folder for images could not be created, access denied.</source>
+        <translation>No s&apos;ha pogut crear la carpeta d&apos;imatges, accés denegat.</translation>
+    </message>
+    <message>
+        <source>Document is not supported.</source>
+        <translation>Document no suportat.</translation>
+    </message>
+    <message>
+        <source>Cannot import. File not found. Already imported?</source>
+        <translation>No es pot importar. No s&apos;ha trobat el document. S&apos;havia importat ja?</translation>
+    </message>
+    <message>
+        <source>Cannot import document, supplied placement nodeID is not valid.</source>
+        <translation>No es pot importar el document, la ubicació de la ID del nòdul no és vàlida.</translation>
+    </message>
+    <message>
+        <source>Cannot store uploaded file, cannot import.</source>
+        <translation>No es pot desar el document carregat, no es pot importar.</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezodf/popupmenu</name>
+    <message>
+        <source>OpenOffice.org</source>
+        <translation>OpenOffice.org</translation>
+    </message>
+    <message>
+        <source>Export OpenOffice</source>
+        <translation>Exporta l&apos;OpenOffice</translation>
+    </message>
+    <message>
+        <source>Export PDF</source>
+        <translation>Exporta PDF</translation>
+    </message>
+    <message>
+        <source>Export Word</source>
+        <translation>Exporta el Word</translation>
+    </message>
+    <message>
+        <source>Import OpenOffice</source>
+        <translation>Importa OpenOffice</translation>
+    </message>
+    <message>
+        <source>Replace OpenOffice</source>
+        <translation>Substitueix l&apos;OpenOffice</translation>
+    </message>
+</context>
+</TS>
