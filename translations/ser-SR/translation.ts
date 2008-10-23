3e2fd69a0ba84fd6352166a11e30df90d9963caa
Lukasz Serwatka <ls@ez.no>
2008-10-23 12:23:12 +0000

    - Added Serbian translation
    git-svn-id: http://svn.ez.no/svn/extensions/ezodf/trunk@3276 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

---
+++ translations/ser-SR/translation.ts
@@ -0,0 +1,224 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>extension/ezodf</name>
+    <message>
+        <source>Upload file</source>
+        <translation>Otpremi datoteku</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Greška</translation>
+    </message>
+    <message>
+        <source>Export Object</source>
+        <translation>Izvezi objekat</translation>
+    </message>
+    <message>
+        <source>OpenOffice.org import</source>
+        <translation>OpenOffice.org uvoz</translation>
+    </message>
+    <message>
+        <source>OpenOffice.org export</source>
+        <translation>OpenOffice.org izvoz</translation>
+    </message>
+    <message>
+        <source>Document is now imported</source>
+        <translation>Dokument je sada uvezen</translation>
+    </message>
+    <message>
+        <source>The object was imported as: %class_name</source>
+        <translation>Objekat je uvezen kao: %class_name</translation>
+    </message>
+    <message>
+        <source>Document imported as</source>
+        <translation>Dokument uvezen kao</translation>
+    </message>
+    <message>
+        <source>Import another document</source>
+        <translation>Uvezi drugi dokument</translation>
+    </message>
+    <message>
+        <source>Import OpenOffice.org document</source>
+        <translation>Uvezi OpenOffice.org dokument</translation>
+    </message>
+    <message>
+        <source>Replace document</source>
+        <translation>Zameni dokument</translation>
+    </message>
+    <message>
+        <source>Import to</source>
+        <translation>Uvezi u</translation>
+    </message>
+    <message>
+        <source>Export eZ Publish content to OpenOffice.org</source>
+        <translation>Izvezi eZ Publish sadržaj u OpenOffice.org</translation>
+    </message>
+    <message>
+        <source>Here you can export any eZ Publish content object to an OpenOffice.org Writer document format.</source>
+        <translation>Ovde možete izvesti bilo koji eZ Publish sadržajni objekat u OpenOffice.org Writer formatu dokumenta.</translation>
+    </message>
+    <message>
+        <source>The images are placed in the media library and can be re-used.</source>
+        <translation>Slike su stavljene u biblioteku medija i mogu se ponovo koristiti.</translation>
+    </message>
+    <message>
+        <source>You can import OpenOffice.org Writer documents directly into eZ Publish from this page. You are
+asked where to place the document and eZ Publish does the rest. The document is converted into
+the appropriate class during the import, you get a notice about this after the import is done.
+Images are placed in the media library so you can re-use them in other articles.</source>
+        <translation>Možete uvesti OpenOffice.org Writer dokumenta direktno u eZ Publish sa ove strane. Pitaće Vas
+gde želite da stavite dokument i eZ Publish će uraditi ostalo. Dokument je adaptiran u odgovarajuću
+klasu prilikom uvoza, a Vi ćete dobiti obaveštenje o ovome nakon što se završi sa uvozom.
+Slike su stavljene u biblioteku medija tako da ih možete ponovo koristiti i u drugim člancima.
+</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezodf/browse</name>
+    <message>
+        <source>Choose document placement</source>
+        <translation>Izaberite mesto dokumenta</translation>
+    </message>
+    <message>
+        <source>Please choose the placement for the OpenOffice.org object.
+
+    Select the placements and click the %buttonname button.
+    Using the recent and bookmark items for quick placement is also possible.
+    Click on placement names to change the browse listing.</source>
+        <translation>Molimo Vas izaberite mesto za OpenOffice.org objekat.
+
+    Izaberite mesto i kliknite na %buttonname dugme.
+    Za brzo pozicioniranje je takođe moguće koristiti nedavne elemente i elemente oznaka za knjigu.
+    Kliknite na položaj imena da biste promenili redosled listinga.</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation>Izaberi</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezodf/export/error</name>
+    <message>
+        <source>Destination file format not supported</source>
+        <translation>Format odredišne datoteke nije podržan</translation>
+    </message>
+    <message>
+        <source>PDF conversion failed</source>
+        <translation>PDF konverzija nije uspela</translation>
+    </message>
+    <message>
+        <source>Word conversion failed</source>
+        <translation>Word konverzija nije uspela</translation>
+    </message>
+    <message>
+        <source>Unable to fetch node, or no read access</source>
+        <translation>Nije moguće dohvatiti čvor ili ga nije moguće pročitati</translation>
+    </message>
+    <message>
+        <source>Unable to open file %1 on server side</source>
+        <translation>Nije moguće otvoriti datoteku %1 na strani servera</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezodf/import/error</name>
+    <message>
+        <source>File extension or type is not allowed.</source>
+        <translation>Oznaka ili tip datoteke nisu dozvoljeni.</translation>
+    </message>
+    <message>
+        <source>Could not parse XML.</source>
+        <translation>Nije moguće analizirati XLM.</translation>
+    </message>
+    <message>
+        <source>Can not open socket. Please check if extension/ezodf/daemon.php is running.</source>
+        <translation>Nije moguće otvoriti spojnicu. Molimo proverite da li extension/ezodf/daemon.php radi.</translation>
+    </message>
+    <message>
+        <source>Can not convert the given document.</source>
+        <translation>Nije moguće pretvoriti dati dokument.</translation>
+    </message>
+    <message>
+        <source>Unable to call daemon. Fork can not create child process.</source>
+        <translation>Nije moguće pozvati daemon. Ogranak ne može da kreira podređeni proces.</translation>
+    </message>
+    <message>
+        <source>Daemon reported error.</source>
+        <translation>Daemon je prijavio grešku.</translation>
+    </message>
+    <message>
+        <source>Unknown node.</source>
+        <translation>Nepoznati čvor.</translation>
+    </message>
+    <message>
+        <source>Access denied.</source>
+        <translation>Pristup nije dozvoljen.</translation>
+    </message>
+    <message>
+        <source>Error during import.</source>
+        <translation>Greška prilikom uvoza.</translation>
+    </message>
+    <message>
+        <source>Unknown content class specified in odf.ini:</source>
+        <translation>Klasa nepoznatog sadržaja navedena u odf.ini:</translation>
+    </message>
+    <message>
+        <source>Unknown error.</source>
+        <translation>Nepoznata greška.</translation>
+    </message>
+    <message>
+        <source>Filetype: </source>
+        <translation>Tip datoteke:</translation>
+    </message>
+    <message>
+        <source>Unable to fetch node with id  </source>
+        <translation>Nije moguće dohvatiti čvor sa id</translation>
+    </message>
+    <message>
+        <source>Folder for images could not be created, access denied.</source>
+        <translation>Nije moguće kreirati fasciklu za slike, pristup nije dozvoljen.</translation>
+    </message>
+    <message>
+        <source>Cannot import. File not found. Already imported?</source>
+        <translation>Ne mogu da uvezem. Datoteka nije pronađena. Već uvezena?</translation>
+    </message>
+    <message>
+        <source>Cannot import document, supplied placement nodeID is not valid.</source>
+        <translation>Ne mogu da uvezem dokument, navedeni položaj IDčvora nije validan.</translation>
+    </message>
+    <message>
+        <source>Cannot store uploaded file, cannot import.</source>
+        <translation>Ne mogu da sačuvam otpremljenu datoteku, ne mogu da uvezem. </translation>
+    </message>
+    <message>
+        <source>Document is not supported.</source>
+        <translation>Dokument nije podržan.</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezodf/popupmenu</name>
+    <message>
+        <source>Export PDF</source>
+        <translation>Izvezi PDF</translation>
+    </message>
+    <message>
+        <source>Export Word</source>
+        <translation>Izvezi Word</translation>
+    </message>
+    <message>
+        <source>OpenOffice.org</source>
+        <translation>OpenOffice.org</translation>
+    </message>
+    <message>
+        <source>Export OpenOffice</source>
+        <translation>Izvezi OpenOffice</translation>
+    </message>
+    <message>
+        <source>Import OpenOffice</source>
+        <translation>Uvezi OpenOffice</translation>
+    </message>
+    <message>
+        <source>Replace OpenOffice</source>
+        <translation>Zameni OpenOffice</translation>
+    </message>
+</context>
+</TS>
