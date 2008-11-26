41750bc9eb9b8aec96b5c83cdc79744e939fd84a
Lukasz Serwatka <ls@ez.no>
2008-11-26 09:44:33 +0000

    - Added fin-FI translation
    git-svn-id: http://svn.ez.no/svn/extensions/ezodf/trunk@3351 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

---
+++ translations/fin-FI/translation.ts
@@ -0,0 +1,228 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>extension/ezodf</name>
+    <message>
+        <source>Upload file</source>
+        <translation>Lataa tiedosto</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Virhe</translation>
+    </message>
+    <message>
+        <source>Export Object</source>
+        <translation>Vie objekti</translation>
+    </message>
+    <message>
+        <source>OpenOffice.org import</source>
+        <translation>OpenOffice.org -tuonti</translation>
+    </message>
+    <message>
+        <source>OpenOffice.org export</source>
+        <translation>OpenOffice.org -vienti</translation>
+    </message>
+    <message>
+        <source>Document is now imported</source>
+        <translation>Asiakirja on nyt siirretty</translation>
+    </message>
+    <message>
+        <source>The object was imported as: %class_name</source>
+        <translation>Objekti tuotiin muodossa %class_name</translation>
+    </message>
+    <message>
+        <source>Document imported as</source>
+        <translation>Asiakirja tuotiin muodossa</translation>
+    </message>
+    <message>
+        <source>Import another document</source>
+        <translation>Tuo toinen asiakirja</translation>
+    </message>
+    <message>
+        <source>Import OpenOffice.org document</source>
+        <translation>Tuo OpenOffice.org -asiakirja</translation>
+    </message>
+    <message>
+        <source>Replace document</source>
+        <translation>Korvaa asiakirja</translation>
+    </message>
+    <message>
+        <source>Import to</source>
+        <translation>Tuo</translation>
+    </message>
+    <message>
+        <source>Export eZ Publish content to OpenOffice.org</source>
+        <translation>Vie eZ Publish -sisältö OpenOffice.org:iin</translation>
+    </message>
+    <message>
+        <source>Here you can export any eZ Publish content object to an OpenOffice.org Writer document format.</source>
+        <translation>Täällä voit muuttaa minkä tahansa eZ Publish sisältöobjektin OpenOffice.org Writer-asiakirjamuotoon.</translation>
+    </message>
+    <message>
+        <source>The images are placed in the media library and can be re-used.</source>
+        <translation>Kuvat on sijoitettu mediakirjastoon ja niitä voi käyttää uudelleen.</translation>
+    </message>
+    <message>
+        <source>You can import OpenOffice.org Writer documents directly into eZ Publish from this page. You are
+asked where to place the document and eZ Publish does the rest. The document is converted into
+the appropriate class during the import, you get a notice about this after the import is done.
+Images are placed in the media library so you can re-use them in other articles.</source>
+        <translation>Voit tuoda OpenOffice.org Writer-asiakirjat suoraan eZ Publish -ohjelmaan käyttämällä tätä sivua. Sinulta
+kysytään minne haluat sijoittaa asiakirjan ja eZ Publish tekee loput. Asiakirja käännetään
+oikeaan luokkaan tuonnin aikana ja saat ilmoituksen, kun vienti on valmis.
+Kuvat on sijoitettu mediakirjastoon, joten voit käyttää niitä uudelleen muissa artikkeleissa.</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezodf/browse</name>
+    <message>
+        <source>Choose document placement</source>
+        <translation>Valitse asiakirjan sijoituspaikka</translation>
+    </message>
+    <message>
+        <source>Please choose the placement for the OpenOffice.org object.
+
+    Select the placements and click the %buttonname button.
+    Using the recent and bookmark items for quick placement is also possible.
+    Click on placement names to change the browse listing.</source>
+        <translation>Valitse OpenOffice.org-objektille sijoituspaikka.
+
+    Valitse sijoituspaikat ja napsauta %buttonname-painiketta.
+    Voit myös hyödyntää viimeisimmät tiedostot - ja kirjainmerkit-valikkoja nopean sijoittamisen apuna.
+    Napsauta sijaintipaikkojen nimiä vaihtaaksesi selauslistaa.
+
+
+
+
+</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation>Valitse</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezodf/export/error</name>
+    <message>
+        <source>Destination file format not supported</source>
+        <translation>Kohdetiedoston formaattia ei tueta</translation>
+    </message>
+    <message>
+        <source>PDF conversion failed</source>
+        <translation>PDF-muunnos epäonnistui</translation>
+    </message>
+    <message>
+        <source>Word conversion failed</source>
+        <translation>Word-muunnos epäonnistui</translation>
+    </message>
+    <message>
+        <source>Unable to fetch node, or no read access</source>
+        <translation>Solmun hakeminen epäonnistui, tai ei lukuoikeuksia</translation>
+    </message>
+    <message>
+        <source>Unable to open file %1 on server side</source>
+        <translation>Tiedoston %1 avaus epäonnistui palvelimen puolelta</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezodf/import/error</name>
+    <message>
+        <source>File extension or type is not allowed.</source>
+        <translation>Tiedostomuotonimikettä tai -tyyppiä ei tueta.</translation>
+    </message>
+    <message>
+        <source>Could not parse XML.</source>
+        <translation>XML-tietoja ei voitu jäsentää.</translation>
+    </message>
+    <message>
+        <source>Can not open socket. Please check if extension/ezodf/daemon.php is running.</source>
+        <translation>Kantaa ei voitu avata. Tarkista jos extension/ezodf/daemon.php on käynnissä.</translation>
+    </message>
+    <message>
+        <source>Can not convert the given document.</source>
+        <translation>Annettua asiakirjaa ei voitu muuntaa.</translation>
+    </message>
+    <message>
+        <source>Unable to call daemon. Fork can not create child process.</source>
+        <translation>Daemonia ei voitu kutsua. Fork ei voi luoda lapsiprosessia.</translation>
+    </message>
+    <message>
+        <source>Daemon reported error.</source>
+        <translation>Daemon ilmoitti virheestä.</translation>
+    </message>
+    <message>
+        <source>Unknown node.</source>
+        <translation>Tuntematon solmu.</translation>
+    </message>
+    <message>
+        <source>Access denied.</source>
+        <translation>Pääsy evätty.</translation>
+    </message>
+    <message>
+        <source>Error during import.</source>
+        <translation>Virhe tuonnin yhteydessä.</translation>
+    </message>
+    <message>
+        <source>Unknown content class specified in odf.ini:</source>
+        <translation>Tuntematon sisältöluokka määritelty tiedostossa odf.ini:</translation>
+    </message>
+    <message>
+        <source>Unknown error.</source>
+        <translation>Tuntematon virhe.</translation>
+    </message>
+    <message>
+        <source>Filetype: </source>
+        <translation>Tiedostotyyppi:</translation>
+    </message>
+    <message>
+        <source>Unable to fetch node with id  </source>
+        <translation>Solmun hakeminen ei mahdollista tunnuksella  </translation>
+    </message>
+    <message>
+        <source>Folder for images could not be created, access denied.</source>
+        <translation>Kansiota kuville ei voitu luoda, pääsy evätty.</translation>
+    </message>
+    <message>
+        <source>Cannot import. File not found. Already imported?</source>
+        <translation>Vienti ei onnistu. Tiedostoa ei löytynyt. Onko se viety jo?</translation>
+    </message>
+    <message>
+        <source>Cannot import document, supplied placement nodeID is not valid.</source>
+        <translation>Asiakirjaa ei voitu viedä, annettu sijoitussolmuID ei ole sopiva.</translation>
+    </message>
+    <message>
+        <source>Cannot store uploaded file, cannot import.</source>
+        <translation>Ladattua tiedostoa ei voida varastoida, tuonti ei onnistu.</translation>
+    </message>
+    <message>
+        <source>Document is not supported.</source>
+        <translation>Asiakirjaa ei tueta.</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezodf/popupmenu</name>
+    <message>
+        <source>Export PDF</source>
+        <translation>Vie PDF</translation>
+    </message>
+    <message>
+        <source>Export Word</source>
+        <translation>Vie Word</translation>
+    </message>
+    <message>
+        <source>OpenOffice.org</source>
+        <translation>OpenOffice.org</translation>
+    </message>
+    <message>
+        <source>Export OpenOffice</source>
+        <translation>Vie OpenOffice</translation>
+    </message>
+    <message>
+        <source>Import OpenOffice</source>
+        <translation>Tuo OpenOffice</translation>
+    </message>
+    <message>
+        <source>Replace OpenOffice</source>
+        <translation>Korvaa OpenOffice</translation>
+    </message>
+</context>
+</TS>
