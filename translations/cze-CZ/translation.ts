9b116d5485d037f9e315850533e4c76dbf855fa7
Lukasz Serwatka <ls@ez.no>
2008-11-26 12:25:41 +0000

    - Added cze-CZ translation
    git-svn-id: http://svn.ez.no/svn/extensions/ezodf/trunk@3358 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

---
+++ translations/cze-CZ/translation.ts
@@ -0,0 +1,221 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>extension/ezodf</name>
+    <message>
+        <source>OpenOffice.org export</source>
+        <translation>OpenOffice.org export</translation>
+    </message>
+    <message>
+        <source>Export eZ Publish content to OpenOffice.org</source>
+        <translation>Exportovat obsah eZ Publish do OpenOffice.org</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Chyba</translation>
+    </message>
+    <message>
+        <source>Here you can export any eZ Publish content object to an OpenOffice.org Writer document format.</source>
+        <translation>Zde můžete exportovat jakýkoli předmět obsahu eZ Publish do formátu dokumentu OpenOffice.org Writer.</translation>
+    </message>
+    <message>
+        <source>Export Object</source>
+        <translation>Exportovat Předmět</translation>
+    </message>
+    <message>
+        <source>Document is now imported</source>
+        <translation>Dokument je nyní importovaný</translation>
+    </message>
+    <message>
+        <source>OpenOffice.org import</source>
+        <translation>Import OpenOffice.org</translation>
+    </message>
+    <message>
+        <source>The object was imported as: %class_name</source>
+        <translation>Předmět byl importovaný jako: %class_name</translation>
+    </message>
+    <message>
+        <source>Document imported as</source>
+        <translation>Dokument importovaný jako</translation>
+    </message>
+    <message>
+        <source>The images are placed in the media library and can be re-used.</source>
+        <translation>Obrázky jsou vkládány do mediální knihovny a mohou býti znovu použity.</translation>
+    </message>
+    <message>
+        <source>Import another document</source>
+        <translation>Importovat další dokument</translation>
+    </message>
+    <message>
+        <source>Upload file</source>
+        <translation>Nahrát soubor</translation>
+    </message>
+    <message>
+        <source>Import OpenOffice.org document</source>
+        <translation>Importovat OpenOffice.org dokument</translation>
+    </message>
+    <message>
+        <source>Replace document</source>
+        <translation>Nahradit dokument</translation>
+    </message>
+    <message>
+        <source>Import to</source>
+        <translation>Importovat do</translation>
+    </message>
+    <message>
+        <source>You can import OpenOffice.org Writer documents directly into eZ Publish from this page. You are
+asked where to place the document and eZ Publish does the rest. The document is converted into
+the appropriate class during the import, you get a notice about this after the import is done.
+Images are placed in the media library so you can re-use them in other articles.</source>
+        <translation>Můžete importovat dokumenty OpenOffice.org Writer přímo do eZ Publish z této stránky. Jste optáni kam umístit dokument a eZ Publish se postará o zbytek. Dokument je  převeden do odpovídající třídy během importu, poté co je import hotový, obdržíte o tom zprávu. Obrázky jsou umístěny v mediální knihovně, tudíž je můžete znovu použít v dalších článcích.</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezodf/browse</name>
+    <message>
+        <source>Choose document placement</source>
+        <translation>Vybrat umístění dokumentu</translation>
+    </message>
+    <message>
+        <source>Please choose the placement for the OpenOffice.org object.
+
+    Select the placements and click the %buttonname button.
+    Using the recent and bookmark items for quick placement is also possible.
+    Click on placement names to change the browse listing.</source>
+        <translation>Prosím vyberte umístění pro předmět OpenOffice.org.
+Vyberte umístění a klikněte na %buttonname tlačítko.
+Pro rychlé umístění je také možné použít již zmíněné a záložky
+Klikněte na uspořádání jmen pro změnu prohlížení seznamu.
+</translation>
+    </message>
+    <message>
+        <source>Select</source>
+        <translation>Vybrat</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezodf/export/error</name>
+    <message>
+        <source>Destination file format not supported</source>
+        <translation>Není podporován cílový formát souboru
+</translation>
+    </message>
+    <message>
+        <source>PDF conversion failed</source>
+        <translation>PDF konverze selhala</translation>
+    </message>
+    <message>
+        <source>Word conversion failed</source>
+        <translation>Word konverze selhala</translation>
+    </message>
+    <message>
+        <source>Unable to fetch node, or no read access</source>
+        <translation>Není možné dosáhnout uzlu nebo přečíst přístup</translation>
+    </message>
+    <message>
+        <source>Unable to open file %1 on server side</source>
+        <translation>Není možné otevřít file %1 na straně serveru</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezodf/import/error</name>
+    <message>
+        <source>File extension or type is not allowed.</source>
+        <translation>Není povolen typ nebo přípona souboru.</translation>
+    </message>
+    <message>
+        <source>Could not parse XML.</source>
+        <translation>Není možné analyzovat XML.</translation>
+    </message>
+    <message>
+        <source>Can not open socket. Please check if extension/ezodf/daemon.php is running.</source>
+        <translation>Nemůže otevřít port. Prosím zkontrolujte, zda funguje extension/ezodf/daemon.php.</translation>
+    </message>
+    <message>
+        <source>Can not convert the given document.</source>
+        <translation>Nemůže konvertovat daný dokument.</translation>
+    </message>
+    <message>
+        <source>Unable to call daemon. Fork can not create child process.</source>
+        <translation>Není možné se spojit s daemon. Fork nemůže vytvořit podřízený proces.</translation>
+    </message>
+    <message>
+        <source>Daemon reported error.</source>
+        <translation>Daemon nahlásil chybu.</translation>
+    </message>
+    <message>
+        <source>Unknown node.</source>
+        <translation>Neznámý uzel.</translation>
+    </message>
+    <message>
+        <source>Access denied.</source>
+        <translation>Přístup odmítnut.</translation>
+    </message>
+    <message>
+        <source>Error during import.</source>
+        <translation>Chyba během importu.</translation>
+    </message>
+    <message>
+        <source>Unknown content class specified in odf.ini:</source>
+        <translation>Neznámá kvalita obsahu specifikovaná v odf. ini:</translation>
+    </message>
+    <message>
+        <source>Unknown error.</source>
+        <translation>Neznámá chyba.</translation>
+    </message>
+    <message>
+        <source>Filetype: </source>
+        <translation>Tip souboru:</translation>
+    </message>
+    <message>
+        <source>Unable to fetch node with id  </source>
+        <translation>Není možné dosáhnout uzlu s id</translation>
+    </message>
+    <message>
+        <source>Folder for images could not be created, access denied.</source>
+        <translation>Složka pro obrázky nemůže být vytvořena, přístup byl odmítnut.</translation>
+    </message>
+    <message>
+        <source>Document is not supported.</source>
+        <translation>Dokument není podporován.</translation>
+    </message>
+    <message>
+        <source>Cannot import. File not found. Already imported?</source>
+        <translation>Nemůže importovat. Soubor nenalezen. Již importováno?</translation>
+    </message>
+    <message>
+        <source>Cannot import document, supplied placement nodeID is not valid.</source>
+        <translation>Nemůže importovat dokument, podporované umístění uzluID není platné.</translation>
+    </message>
+    <message>
+        <source>Cannot store uploaded file, cannot import.</source>
+        <translation>Nemůže uložit nahraný soubor, nemůže importovat.</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezodf/popupmenu</name>
+    <message>
+        <source>OpenOffice.org</source>
+        <translation>OpenOffice.org</translation>
+    </message>
+    <message>
+        <source>Export OpenOffice</source>
+        <translation>Exportovat OpenOffice</translation>
+    </message>
+    <message>
+        <source>Export PDF</source>
+        <translation>Exportovat PDF</translation>
+    </message>
+    <message>
+        <source>Export Word</source>
+        <translation>Exportovat Word</translation>
+    </message>
+    <message>
+        <source>Import OpenOffice</source>
+        <translation>Importovat OpenOffice</translation>
+    </message>
+    <message>
+        <source>Replace OpenOffice</source>
+        <translation>Naradit OpenOffice</translation>
+    </message>
+</context>
+</TS>
