cf2b106aa1e8054e280f7133d562728ec6a37c12
Kristof Coomans <kc@ez.no>
2009-01-20 09:18:56 +0000

    - Implemented enhancement #14230: Use the Archive Component in ezodf # ezinfo.php should no longer mention old library
    git-svn-id: http://svn.ez.no/svn/extensions/ezodf/trunk@3483 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- ezinfo.php
+++ ezinfo.php
@@ -30,12 +30,8 @@ class ezodfInfo
     {
         return array( 'Name' => "eZ OpenOffice.org extension",
                       'Version' => "2.3.0",
-                      'Copyright' => "Copyright (C) 1999-2006 eZ systems AS",
+                      'Copyright' => "Copyright (C) 1999-2009 eZ Systems AS",
                       'License' => "GNU General Public License v2.0",
-                      'Includes the following third-party software' => array( 'Name' => 'PhpConcept Library - Zip Module',
-                                                                              'Version' => '2.4',
-                                                                              'License' => 'GNU/LGPL - Vincent Blavet - November 2004',
-                                                                              'For more information' => 'http://www.phpconcept.net' )
                       );
     }
 }
