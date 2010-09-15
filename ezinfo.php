213148577ef908646595ba26d496fa17958ac9c5
Ole Marius Smestad <oms@ez.no>
2010-09-15 14:21:24 +0000

    - Updated: Copyright and license info.
    git-svn-id: http://svn.ez.no/svn/extensions/ezodf/stable/2.4@4820 49f50cf4-cce6-0310-bc1e-a21d999f3d8f

--- ezinfo.php
+++ ezinfo.php
@@ -30,7 +30,7 @@ class ezodfInfo
     {
         return array( 'Name' => 'eZ OpenOffice.org extension',
                       'Version' => '2.4.0',
-                      'Copyright' => 'Copyright (C) 1999-' . date( 'Y' ) . ' eZ Systems AS',
+                      'Copyright' => 'Copyright (C) 1999-2010 eZ Systems AS',
                       'License' => 'GNU General Public License v2.0',
                       );
     }
